a12b68562399fe3bc7908fb04f88cefd09d30a5b
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2024-11-27 14:54:56 +0300

    Updated on header

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,18 +1,35 @@
 // Header.js
-import  { useState } from "react";
-import '../Css/Header.css'; // Assuming you have the CSS saved in Nav.css
+import { useState } from "react";
+import "../Css/Header.css"; // Assuming you have the CSS saved in Nav.css
 import { Link } from "react-router-dom";
 // import logo from '../assets/logo.png';
 import { useNavigate } from "react-router-dom";
 import UseLogo from "./UseLogo";
 import { API_URL } from "../App";
+import Cookies from "js-cookie";
 const Header = () => {
   const navigate = useNavigate();
   const lang = location.pathname.split("/")[1] || "en";
   const [isOpen, setIsOpen] = useState(false);
   const [isDropdownVisible, setDropdownVisible] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(null)
-  const {logo}=UseLogo(lang)
+  const [selectedOption, setSelectedOption] = useState(null);
+  const { logo } = UseLogo(lang);
+
+  const logout = async () => {
+    try {
+      // const response = await axios.post(`http://localhost:3000/users/logout`, {}, {
+      //   withCredentials: true,
+      // });
+
+      // console.log("Logged out successfully", response.data);
+
+      Cookies.remove("token");
+
+      navigate(`/${lang}/login`);
+    } catch (error) {
+      console.error("Error during logout: ", error);
+    }
+  };
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
@@ -27,52 +44,94 @@ const Header = () => {
   };
   return (
     <nav>
-       {logo.map((log) => (
+      {logo.map((log) => (
         <div className="logo" key={log.id}>
           <img src={`${API_URL}/uploads/${log.image}`} alt="Logo" />
         </div>
       ))}
 
-      <div className={`${lang==='ar' ? "hamburger hamburger_ar":"hamburger"} ${isOpen ? 'toggle' : ''}`} onClick={toggleMenu}>
+      <div
+        className={`${lang === "ar" ? "hamburger hamburger_ar" : "hamburger"} ${
+          isOpen ? "toggle" : ""
+        }`}
+        onClick={toggleMenu}
+      >
         <div className="line1"></div>
         <div className="line2"></div>
         <div className="line3"></div>
       </div>
 
-      <ul className={`nav-links ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        <li><Link to= {`${lang}`}>{lang==='ar'? "الرئيسية":"Home"}        </Link></li>
-        <li><Link to={`${lang}/about`}>{lang==='ar'? "عن حدائق الاردن":" About"}</Link></li>
-        <li><Link to={`${lang}/services`}>{lang==='ar'? "الخدمات":"Services"}
-        </Link></li>
-        <li><Link to={`${lang}/projects`}>{lang==='ar'? "المشاريع":"Projects"}
-        </Link></li>
-        <li><Link to={`${lang}/blogs`}>{lang==='ar'? "المدونات":" Blogs"}
-        </Link></li>
-        <li><Link to={`${lang}/careers`}>{lang==='ar'? "الوظائف":" Careers"}
-        </Link></li>
-        <li><Link to={`${lang}/contact`}>{lang==='ar'? "تواصل معنا":" Contact"}
-        </Link></li>
+      <ul className={`nav-links ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <li>
+          <Link to={`${lang}`}>{lang === "ar" ? "الرئيسية" : "Home"} </Link>
+        </li>
+        <li>
+          <Link to={`${lang}/about`}>
+            {lang === "ar" ? "عن حدائق الاردن" : " About"}
+          </Link>
+        </li>
+        <li>
+          <Link to={`${lang}/services`}>
+            {lang === "ar" ? "الخدمات" : "Services"}
+          </Link>
+        </li>
+        <li>
+          <Link to={`${lang}/projects`}>
+            {lang === "ar" ? "المشاريع" : "Projects"}
+          </Link>
+        </li>
+        <li>
+          <Link to={`${lang}/blogs`}>
+            {lang === "ar" ? "المدونات" : " Blogs"}
+          </Link>
+        </li>
+        <li>
+          <Link to={`${lang}/careers`}>
+            {lang === "ar" ? "الوظائف" : " Careers"}
+          </Link>
+        </li>
+        <li>
+          <Link to={`${lang}/contact`}>
+            {lang === "ar" ? "تواصل معنا" : " Contact"}
+          </Link>
+        </li>
         <div className="d-flex">
-
-        <li><Link to={`/`}><button className="Login-button"> {lang==='ar'? "تسجيل دخول":"Login"}
-        </button></Link></li>
-        <li><Link to={`${lang}/signup`}><button className="Login-button background_btn"> {lang==='ar'? "تسجيل حساب":" Sign Up"}
-        </button></Link></li>
-        <div
-       className="dropdown-container border-none"
-       onClick={toggleDropdown}
-       >
-  <div className="dropdown-wrapper">
-    <select
-      className="form-select small-select"
-      value={selectedOption}
-      onChange={handleSelection}
-    >
-      <option value="en">en</option>
-      <option value="ar">ar</option>
-    </select>
-  </div>
-</div>
+          <li>
+            <Link to={`/`}>
+              <button className="Login-button">
+                {" "}
+                {lang === "ar" ? "تسجيل دخول" : "Login"}
+              </button>
+            </Link>
+          </li>
+          <li>
+            <Link to={`${lang}/signup`}>
+              <button className="Login-button background_btn">
+                {" "}
+                {lang === "ar" ? "تسجيل حساب" : " Sign Up"}
+              </button>
+            </Link>
+          </li>
+          <div
+            className="dropdown-container border-none"
+            onClick={toggleDropdown}
+          >
+            <div className="dropdown-wrapper">
+              <select
+                className="form-select small-select"
+                value={selectedOption}
+                onChange={handleSelection}
+              >
+                <option value="en">en</option>
+                <option value="ar">ar</option>
+              </select>
+            </div>
+            <li>
+              <button className="logout-button" onClick={logout}>
+                {lang === "ar" ? "تسجيل خروج" : "Logout"}
+              </button>
+            </li>
+          </div>
         </div>
       </ul>
     </nav>
