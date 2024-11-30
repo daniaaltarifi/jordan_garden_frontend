dd57e42ba03426e00c76d38ee24d982ef9eb3dee
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2024-11-30 09:58:16 +0300

    Updated On front end

--- src/Pages/JobDescription.jsx
+++ src/Pages/JobDescription.jsx
@@ -23,7 +23,7 @@ function JobDescription() {
 
   useEffect(() => {
     axios
-      .get(`${API_URL}/careersdescription/getcareerdescriptionbyid/${careerid}/${lang}`)
+      .get(`${API_URL}/careersdescription/getcareerdescriptionbycareer_id/${careerid}/${lang}`)
       .then((response) => {
         setJobDescription(response.data);
       })
@@ -33,6 +33,7 @@ function JobDescription() {
   }, [careerid, lang]);
 
   const handleSubmit = (event) => {
+    event.preventDefault()
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -58,7 +59,6 @@ function JobDescription() {
       .then((response) => {
         console.log("Career created successfully:", response.data);
         
-      
         Swal.fire({
           title: "The Job is",
           text: "The job application has been successfully submitted.",
